08b7c89add5826bda5cc84b86a6ab5277adec9b7
Orange <94119778+Riysin@users.noreply.github.com>
2026-07-06 04:55:08 +0800

    Chordify reader v4

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "chordify-v4";
+const SHELL = ["./", "index.html", "manifest.webmanifest",
+               "icon-192.png", "icon-512.png", "icon-180.png"];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(() => {}));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith("songs.json")) {
+    e.respondWith(fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
