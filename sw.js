34fcf02d40ee84e7a2914d6a38e2cd506e6c88b1
Orange <94119778+Riysin@users.noreply.github.com>
2026-07-15 04:37:54 +0800

    Chordify reader v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chordify-v6";
+const CACHE = "chordify-v8";
 const SHELL = ["./", "index.html", "manifest.webmanifest",
                "icon-192.png", "icon-512.png", "icon-180.png"];
 self.addEventListener("install", e => {
